ec1ecdb049d1a96cd7a768e3c5d081e14b579809
Ludovic Launer <ludovic.launer@wika.com>
2020-04-14 09:35:55 +0200

    Simplified version to deal with airspace only

--- index.js
+++ index.js
@@ -5,17 +5,11 @@ var xml2js		= require('xml2js'),
 	Q			= require('q'),
 	GeoJSON		= require('geojson');
 // Declare global vars
-var airports	= [],
-	runways		= [],
-	airspaces	= [],
-	navaids		= [],
-	hotspots	= [],
+var airspaces	= [],
+
 	pathInput   = "./input/",
-	pathOutput	= "./output/",
-    points      = [],
-    polygons    = [],
-    fileTypes   = ['airports', 'airspace', 'hotspot', 'navaid'],
-    geoBlocks   = ['airports', 'airspaces', 'hotspots', 'navaids', 'runways'];
+	pathOutput	= "./output/"
+
 
 init();
 
@@ -26,88 +20,14 @@ function init(){
 	console.log(colors.green(">>> reading [input] folder"));
 	fs.readdir(pathInput, function(err, items) {
 		for (var i=0; i<items.length; i++) {
-			for(var j=0; j < fileTypes.length; j++){
-				if(items[i].indexOf(fileTypes[j]) !== -1){
-					getFileData(items[i], fileTypes[j]);
-				}
-			}
+            if(items[i].indexOf('airspace') !== -1){
+                getFileData(items[i]);
+            }
 		}
 	});
 }
 
-function analyseDatas(data, block){
-	console.log(colors.yellow('>>> Staring analysis of ' + block));
-	switch (block) {
-		case 'airports'	: doAirports(data, block); break;
-		case 'airspace'	: doAirspaces(data, block); break;
-		case 'hotspot'	: doHotspot(data, block); break;
-		case 'navaid'	: doNavaid(data, block); break;
-	}
-}
-
-function doAirports(inputData, block){
-    var airportList = inputData.OPENAIP.WAYPOINTS[0].AIRPORT;
-    for(var a = 0; a < airportList.length; a++){
-        var tempAirport = airportList[a];
-        var airport = {
-            guid:       "",
-            aeronautical : "airport",
-            name:       tempAirport.NAME[0],
-            type:       tempAirport.$.TYPE,
-            country:    tempAirport.COUNTRY[0],
-            icao :      (tempAirport.ICAO != undefined ) ? tempAirport.ICAO[0] : "",
-            radios:     [],
-            latitude :  parseFloat(tempAirport.GEOLOCATION[0].LAT[0]),
-            longitude : parseFloat(tempAirport.GEOLOCATION[0].LON[0]),
-            elev :      parseFloat(tempAirport.GEOLOCATION[0].ELEV[0]._)
-        };
-        // Work on radios
-        if(tempAirport.RADIO != undefined){
-            for(var r = 0; r < tempAirport.RADIO.length; r++){
-                var tempRadio = tempAirport.RADIO[r];
-                var radio = {
-                    category: tempRadio.$.CATEGORY,
-                    frequency: parseFloat(tempRadio.FREQUENCY[0]),
-                    type: tempRadio.TYPE[0],
-                    spec: (tempRadio.TYPESPEC != undefined ) ? tempRadio.TYPESPEC[0] : "",
-                    description: (tempRadio.DESCRIPTION != undefined) ? tempRadio.DESCRIPTION[0] : ""
-                };
-                airport.radios.push(radio);
-            }
-        }
-        // Work on runways
-        if(tempAirport.RWY != undefined){
-            for(var r = 0; r < tempAirport.RWY.length; r++){
-                var tempRnw = tempAirport.RWY[r];
-                var rnw = {
-                    guid:       "",
-                    aeronautical : "runway",
-                    operations : tempRnw.$.OPERATIONS,
-                    airport : airport.name,
-                    name : tempRnw.NAME[0],
-                    sfc : tempRnw.SFC[0],
-                    latitude :  airport.latitude,
-                    longitude : airport.longitude,
-                    elev :      airport.elev,
-                    length : parseFloat(tempRnw.LENGTH[0]._),
-                    width : parseFloat(tempRnw.WIDTH[0]._),
-                    directions: []
-                };
-                if( tempRnw.DIRECTION != undefined ){
-                    for(var z = 0; z < tempRnw.DIRECTION.length; z++){
-                        rnw.directions.push(tempRnw.DIRECTION[z].$.TC);
-                    }
-                }
-                runways.push(rnw);
-            }
-        }
-        airports.push(airport);
-    }
-    console.log(colors.yellow(">>> DONE : "+ airports.length + " airports"));
-    checkContinueProcess(block);
-}
-
-function doAirspaces(inputData, block){
+function doAirspaces(inputData){
     var airspacesList = inputData.OPENAIP.AIRSPACES[0].ASP;
     for(var a = 0; a < airspacesList.length; a ++){
         var tempAirspace = airspacesList[a];
@@ -144,93 +64,12 @@ function doAirspaces(inputData, block){
         airspaces.push(airspace);
     }
     console.log(colors.yellow(">>> DONE : "+ airspaces.length + " airspaces"));
-    checkContinueProcess(block);
+
+    // Create file
+    createGeoFile(airspaces);
 }
 
-function doHotspot(inputData, block){
-    var hotspotList = inputData.OPENAIP.HOTSPOTS[0].HOTSPOT;
-    for(var h = 0; h < hotspotList.length; h++){
-        var tempHotspot = hotspotList[h];
-        var hotspot = {
-            guid:           "",
-            aeronautical : 	"hotspot",
-            type:           tempHotspot.$.TYPE,
-            country:        tempHotspot.COUNTRY[0],
-            name:           tempHotspot.NAME[0],
-            latitude :      parseFloat(tempHotspot.GEOLOCATION[0].LAT[0]),
-            longitude :     parseFloat(tempHotspot.GEOLOCATION[0].LON[0]),
-            elev :          parseFloat(tempHotspot.GEOLOCATION[0].ELEV[0]._),
-            reliability:    parseFloat(tempHotspot.RELIABILITY[0]),
-            occurrence:     tempHotspot.OCCURRENCE[0],
-            conditions:[]
-        };
-        // Working on conditions.
-        if(tempHotspot.CONDITIONS != undefined){
-            for(var i = 0; i < tempHotspot.CONDITIONS.length; i++){
-                var tmpCond = tempHotspot.CONDITIONS[i];
-                var cond = {
-                    type: tmpCond.$.TYPE,
-                    timeofday: [],
-                    wind:[]
-                };
-                // Work on times
-                if(tmpCond.TIMEOFDAY != undefined){
-                    for(var j=0; j < tmpCond.TIMEOFDAY[0].TIME.length; j++){
-                        cond.timeofday.push(tmpCond.TIMEOFDAY[0].TIME[j]);
-                    }
-                }
-                // Work on winds
-                if(tmpCond.WIND != undefined){
-                    for(var j = 0; j < tmpCond.WIND[0].DIRECTION.length; j++){
-                        cond.wind.push(tmpCond.WIND[0].DIRECTION[j]);
-                    }
-                }
-                hotspot.conditions.push(cond);
-            }
-        }
-        hotspots.push(hotspot);
-    }
-    console.log(colors.yellow(">>> DONE : "+ hotspots.length + " hotspots"));
-    checkContinueProcess(block);
-}
-
-function doNavaid(inputData, block){
-    var navaidList = inputData.OPENAIP.NAVAIDS[0].NAVAID;
-    for(var i = 0; i < navaidList.length; i++){
-        var tempNavaid = navaidList[i];
-        var navaid = {
-            guid:       "",
-            aeronautical : "navaid",
-            type:       tempNavaid.$.TYPE,
-            country:    tempNavaid.COUNTRY[0],
-            id:         tempNavaid.ID[0],
-            latitude :  parseFloat(tempNavaid.GEOLOCATION[0].LAT[0]),
-            longitude : parseFloat(tempNavaid.GEOLOCATION[0].LON[0]),
-            elev :      parseFloat(tempNavaid.GEOLOCATION[0].ELEV[0]._),
-            radios:[],
-            params:{
-                range: (tempNavaid.PARAMS[0].RANGE != undefined) ? parseFloat(tempNavaid.PARAMS[0].RANGE[0]._) : null,
-                declination : parseFloat(tempNavaid.PARAMS[0].DECLINATION[0]),
-                alignedtotruenorth : !(tempNavaid.PARAMS[0].ALIGNEDTOTRUENORTH[0] === "FALSE")
-            }
-        };
-        // Work on radio
-        if(tempNavaid.RADIO != undefined){
-            for(var j =0; j < tempNavaid.RADIO.length; j++){
-                var radio = {
-                    frequency:  parseFloat(tempNavaid.RADIO[j].FREQUENCY[0]),
-                    channel:    (tempNavaid.RADIO[j].CHANNEL != undefined) ? tempNavaid.RADIO[j].CHANNEL[0] : ""
-                };
-                navaid.radios.push(radio);
-            }
-        }
-        navaids.push(navaid);
-    }
-    console.log(colors.yellow(">>> DONE : "+ navaids.length + " navaids"));
-    checkContinueProcess(block);
-}
-
-function getFileData(item, block){
+function getFileData(item){
 	var file = pathInput+item;
     var ext = item.slice(-3);
     if( ext === 'aip'){
@@ -238,7 +77,7 @@ function getFileData(item, block){
             if(err){ return console.err(err); }
             xml2js.parseString(data, function(err, result){
                 if(err) { return console.err(err);}
-                return analyseDatas(result, block);
+                return doAirspaces(result);
             });
         });
     } else {
@@ -246,62 +85,10 @@ function getFileData(item, block){
     }
 }
 
-function checkContinueProcess(block){
-    // Set the filtype corresponding to null
-    for(var i = 0; i < fileTypes.length; i++){
-        if(fileTypes[i] === block){
-            fileTypes[i] = null;
-        }
-    }
-    // Checks if a type of datas has been done or not.
-    var t = fileTypes; t.sort();
-
-    if(t[0] == null){
-        startProcess();
-    }
-}
-
-function startProcess(){
-    console.log(colors.green(">>> Pre-work finished, starting generating GeoJSON files"));
-    for(var i = 0; i < geoBlocks.length; i++){
-        var data = null;
-        switch(geoBlocks[i]){
-            case 'airports'	: data = airports; break;
-            case 'airspaces': data = airspaces; break;
-            case 'hotspots'	: data = hotspots; break;
-            case 'navaids'	: data = navaids; break;
-            case 'runways'  : data = runways; break;
-        }
-        createGeoFile(checkDuplicatesInArray(data, geoBlocks[i]), geoBlocks[i]);
-    }
-}
-
-function createGeoFile(data, block){
-    var geoData = GeoJSON.parse(data, ( block !== 'airspaces') ? {Point:['latitude','longitude']} : {'Polygon':'geometry'});
-    fs.writeFile('./output/'+block+'.geojson', JSON.stringify(geoData), (err) => {
+function createGeoFile(data){
+    var geoData = GeoJSON.parse(data, {'Polygon':'geometry'});
+    fs.writeFile('./output/airspace.geojson', JSON.stringify(geoData), (err) => {
         if (err) throw err;
-        console.log(colors.green(">>> Saved geojson file for "+ block));
+        console.log(colors.green(">>> Saved geojson file"));
     });
-}
-
-function checkDuplicatesInArray(data, block){
-    // Creates a duplicate Array, in which unique items will be stored.
-    var tempArray = [];
-    for(var i = 0; i < data.length; i++){
-        var item = data[i];
-        if(tempArray.length == 0){
-            tempArray.push(item);
-        } else {
-            var existsInArray = false;
-            for(var j = 0; j < tempArray.length; j++){
-                if(existsInArray === false){
-                    existsInArray = (tempArray[j] == item);
-                }
-            }
-            if(!existsInArray){
-                tempArray.push(item);
-            }
-        }
-    }
-    return tempArray;
 }
